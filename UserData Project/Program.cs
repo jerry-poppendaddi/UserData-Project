cf726f7e6c6d0911e73a082c86ceb69702736f9c
Ive <ive.alex.89@gmail.com>
2023-03-27 22:15:56 -0400

    Mar 27 commit with user data

--- UserData Project/Program.cs
+++ UserData Project/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{  static (string Name, string LastName, int Age) EnterUser()
+    { (string Name, string LastName, int Age) User;
+
+        Console.WriteLine("What is your First Name?");
+        User.Name = Console.ReadLine();
+
+        Console.WriteLine("What is your Last Name?");
+        User.LastName = Console.ReadLine();
+
+        string age;
+        int intage;
+
+        do
+        {
+            Console.WriteLine("Enter your age as a number:");
+            age = Console.ReadLine();
+        } while (CheckNum(age, out intage));
+
+        User.Age = intage;
+        return User;
+    }
+}
+
