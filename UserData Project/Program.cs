5b6e4805f8b92d7d81c2a4c38e1edad3a11f60f2
Ive <ive.alex.89@gmail.com>
2023-03-31 00:33:53 -0400

    Final Commit!

--- UserData Project/Program.cs
+++ UserData Project/Program.cs
@@ -3,8 +3,14 @@
 class Program
 { 
     static void Main(string[] args)
-    { Console.WriteLine("Hello!");
-       
+    { Console.WriteLine("Hello! Let's do a quick survey to get to know you!Just Press Enter to start.");
+        Console.ReadKey();
+        EnterUser();          //Launching EnterUser Program to collect all data from User
+
+        Console.WriteLine("Thank you for your time!");
+        Console.WriteLine("Goodbye for now!");
+        Console.ReadKey();
+                      
     }
 
     static (string Name, string LastName, int Age, int SumPet, string[] NamePet, string[] FavColors) EnterUser()  //First Method
@@ -57,13 +63,19 @@ class Program
         static string[] NamePets(int SumPet)       //Pet Names Data Collection
         {
             string[] PetList = new string[SumPet];
+            string check;
 
             for (int i = 0; i < PetList.Length; i++)
             {
-                Console.WriteLine("Type your pet #{0} name below:", i + 1);
-                PetList[i] = Console.ReadLine();
+                do
+                {
+                    Console.WriteLine("Type your pet #{0} name below:", i + 1);
+                    check = Console.ReadLine();
+                } while (CheckName(check));
+                PetList[i] = check;
             }
-            return PetList;
+                return PetList;
+            
         }
 
         User.FavColors = ColorList();
@@ -73,9 +85,43 @@ class Program
             string[] favcolor = new string[3];
             for (int k = 0; k < favcolor.Length; k++)
             {
-                Console.WriteLine("Enter your favourite color #{0}", k + 1);
+                Console.WriteLine("Enter your favourite color #{0} (lower case):", k + 1);
                 favcolor[k] = Console.ReadLine();
+                switch(favcolor[k])
+                { case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Noted! Your #{0} color is red!", k + 1);
+                        break;
+                  case "yellow":
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Noted! Your #{0} color is yellow!", k + 1);
+                        break;
+                  case "white":
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Noted! Your #{0} color is white!", k + 1);
+                        break;
+                  case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Noted! Your #{0} color is green!", k + 1);
+                        break;
+                  case "blue":
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Noted! Your #{0} color is blue!", k + 1);
+                        break;
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Sorry, this color not available.");
+                        break;
+                }
             }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
             return favcolor;
         }
 
@@ -107,7 +153,7 @@ class Program
                     return false;
             }
             return true;
-
+            
         }
     
     }
